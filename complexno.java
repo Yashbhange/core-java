7e4a3cb2a6e0fef38c0608f61af559d2eec1a925
Yashbhange <161116467+Yashbhange@users.noreply.github.com>
2025-06-09 02:05:37 -0700

    Create complexno.java

---
+++ complexno.java
@@ -0,0 +1,11 @@
+public class ComplexNumber {
+    public static void main(String[] args) {
+        int real1 = 3, imag1 = 2;
+        int real2 = 1, imag2 = 7;
+
+        int realSum = real1 + real2;
+        int imagSum = imag1 + imag2;
+
+        System.out.println("Sum = " + realSum + " + " + imagSum + "i");
+    }
+}
