f9dd29d6ce96f7c618ade0831ee1893750aae583
Yashbhange <161116467+Yashbhange@users.noreply.github.com>
2025-06-09 02:22:49 -0700

    Create binaryno.java

---
+++ binaryno.java
@@ -0,0 +1,11 @@
+public class binaryno {
+    public static void main(String[]args){
+        
+        int num1 = 12;
+        int num2 = 15;
+
+        int sum = num1+num2;
+        String binarySum = Integer.toBinaryString(sum);
+        System.out.println(binarySum);
+}
+}
